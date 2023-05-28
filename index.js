8b3b36b0d7d7d96f1c83288a84aedd5722dd62cc
Anushka Deshpande <53345232+anushkadeshpande@users.noreply.github.com>
2023-05-28 17:35:16 +0530

    Added hot reload

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow } = require('electron')
 
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
